4b32f0177c938d5ef7d0ab75783afb47e4a25ff1
Alif Fachrel Anargya <aliffachrel@gmail.com>
2025-06-20 13:27:53 +0700

    Add basic encryption utilities and CLI interfaces
    Introduces three new modules: encryptors.js with caesar, symbol, and reverse ciphers; message-mixer.js for a CLI to encrypt messages using these ciphers; and super-encoder.js as a scaffold for encoding/decoding messages via CLI. These utilities provide foundational encryption and user interaction features.

---
+++ src/super-encoder.js
@@ -0,0 +1,21 @@
+
+const encodeMessage = (str) => {};
+
+const decodeMessage = (str) => {};
+
+/* Helper function for displaying the encrypted message to the user. */
+const handleInput = (userInput) => {
+  const str = userInput.toString().trim();
+  let output;
+  if (process.argv[2] === "encode") {
+    output = encodeMessage(str);
+  }
+  if (process.argv[2] === "decode") {
+    output = decodeMessage(str);
+  }
+  process.stdout.write(output + "\n");
+  process.exit();
+};
+
+process.stdout.write("Enter the message you would like to encrypt...\n> ");
+process.stdin.on("data", handleInput);
